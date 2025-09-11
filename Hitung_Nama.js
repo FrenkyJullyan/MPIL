04d20fc22888306e611973a4dbb6dc747d0643ad
Frenky Jullyan <rayaeshalrayda@gmail.com>
2025-09-11 09:58:50 +0700

    Menambahkan tugas menghitung nama di javascrip

---
+++ Hitung_Nama.js
@@ -0,0 +1,13 @@
+// contoh nama
+let nama = "Frenky";
+
+// hitung jumlah huruf
+let jumlahHuruf = nama.length;
+
+// kali 5
+let hasil = jumlahHuruf * 5;
+
+// tampilkan hasil
+console.log("Nama: " + nama);
+console.log("Jumlah huruf: " + jumlahHuruf);
+console.log("Hasil (jumlah huruf * 5): " + hasil);
