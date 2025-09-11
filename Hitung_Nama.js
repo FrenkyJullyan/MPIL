a66527b5ea5f7b182391711a311b6e0bf23a163e
FrenkyJullyan <rayaeshalrayda@gmail.com>
2025-09-11 16:41:14 +0700

    Menambahkan kode cara menghitung nama di JavaScrip

--- Hitung_Nama.js
+++ Hitung_Nama.js
@@ -1,13 +1,7 @@
-// contoh nama
 let nama = "Frenky";
-
-// hitung jumlah huruf
 let jumlahHuruf = nama.length;
-
-// kali 5
 let hasil = jumlahHuruf * 5;
 
-// tampilkan hasil
 console.log("Nama: " + nama);
 console.log("Jumlah huruf: " + jumlahHuruf);
 console.log("Hasil (jumlah huruf * 5): " + hasil);
